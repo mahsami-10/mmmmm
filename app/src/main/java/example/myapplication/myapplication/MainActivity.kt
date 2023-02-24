f73bbce5f659ae41da93214e9bc7e7ad1a8e87dc
mahsa <mmahsamizabi@gmail.com>
2023-02-24 10:52:37 -0800

    added sone cod

--- app/src/main/java/example/myapplication/myapplication/MainActivity.kt
+++ app/src/main/java/example/myapplication/myapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package example.myapplication.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(, "hello android", Toast.LENGTH_SHORT).show()
     }
 }
